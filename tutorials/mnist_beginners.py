3903b5bbaf0da8b99b8b65618e904e4fc6aa0df6
David Dohan <ddohan@cs.princeton.edu>
2016-05-12 16:01:45 -0400

    Went through TF mnist tutorial for basic overview

---
+++ tutorials/mnist_beginners.py
@@ -0,0 +1,36 @@
+# Following intro tutorial: https://www.tensorflow.org/versions/r0.8/tutorials/mnist/beginners/index.html
+# Basically just transcribing to get a feeling for TF
+
+import tensorflow as tf
+
+# Load data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+# Predicted labels
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# Proper labels
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+xent = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(xent)
+
+init = tf.initialize_all_variables()
+
+
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
