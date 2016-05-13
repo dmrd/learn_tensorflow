0a77931cd6700ffae28b4a8cff33bab986c49951
David Dohan <ddohan@cs.princeton.edu>
2016-05-12 20:48:41 -0400

    Crossentropy method works!
    Need to add support for Box spaces now and experiment with params

--- openai/cross_entropy.py
+++ openai/cross_entropy.py
@@ -1,9 +1,18 @@
+import sys
 import math
 import numpy as np
+import gym
 
+if len(sys.argv) > 1:
+    ENV = sys.argv[1]
+    if len(sys.argv) == 3:
+        exp_name = sys.argv[2]
+else:
+    ENV = "CartPole-v0"
+    exp_name = None
 
-class MLP(object):
-    def __init__(self, layer_dims):
+class MLPAgent(object):
+    def __init__(self, layer_dims, ):
         self.layer_dims = layer_dims
         self.weights = []
         for (i, dim) in enumerate(layer_dims[1:]):
@@ -12,9 +21,12 @@ class MLP(object):
 
     def forward(self, x):
         for W in self.weights:
-            x = np.dot(x, W)
+            x = np.tanh(np.dot(x, W))
         return x
 
+    """
+    Get the params as a single vector.  Concatenated weight matrices
+    """
     def get_flattened(self):
         ret = np.zeros(self.nparams)
         start = 0
@@ -24,6 +36,10 @@ class MLP(object):
             start += sz
         return ret
 
+    """
+    Get the weight matrices from a single flattened vector of params.
+    Inverse of get_flattened
+    """
     def set_flattened(self, params):
         assert params.size == self.nparams
         start = 0
@@ -32,20 +48,58 @@ class MLP(object):
             W.flat = params[start:start + sz]
             start += sz
 
+    """
+    Return the action given state
+    Just argmax over possible actions for now (i.e. no stochastic sampling)
+    """
+    def step(self, state):
+        y = self.forward(state)
+        return np.argmax(y)
 
-class Agent(object):
-    def __init__(self, state_dim, action_dim):
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-    def step(state):
-        pass
 
-
-def simulate(env, agent, steps):
+def simulate(env, agent, max_steps, render=False):
     obs = env.reset()
-    for _ in range(steps):
+    total_reward = 0.0
+    for _ in xrange(max_steps):
         action = agent.step(obs)
         obs, reward, done, info = env.step(action)
+        if render:
+            env.render()
+        total_reward += reward
         if done:
             break
-    return reward
+    return total_reward
+
+
+def eval_agent(env, agent, max_steps):
+    def eval_params(params, render=False):
+        agent.set_flattened(params)
+        return simulate(env, agent, max_steps, render=render)
+    return eval_params
+
+def cem(f, iters=10, n_samples=100, keep_percent=0.2, std_noise=0.0, noise_decay=1):
+    means = np.zeros(agent.nparams)
+    stds = np.ones(agent.nparams)
+
+    n_elite = int(round(keep_percent * n_samples))
+    for generation in xrange(iters):
+        stds += std_noise * pow(noise_decay, generation)
+        samples = np.random.multivariate_normal(means, np.diag(stds), n_samples)
+        rewards = np.zeros(n_samples)
+        for i, sample in enumerate(samples):
+            rewards[i] = f(sample, render=(i == 0))
+        best_idx = rewards.argsort()[-n_elite:]
+        elite = samples[best_idx]
+        means = elite.mean(axis=0)
+        stds = elite.var(axis=0)
+
+        print("{}: Average reward: {:10} | elite reward: {:10}".format(
+            generation + 1, rewards.mean(), rewards[best_idx].mean()))
+
+env = gym.make(ENV)
+agent = MLPAgent([sum(env.observation_space.shape), 10, 5, env.action_space.n])
+if exp_name:
+    env.monitor.start(exp_name)
+cem(eval_agent(env, agent, 200), iters=30)
+if exp_name:
+    env.monitor.close()
